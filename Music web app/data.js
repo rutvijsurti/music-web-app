906cb0af7c3a0c1b36e192120ba086d097743526
rutvijsurti <118683016+rutvijsurti@users.noreply.github.com>
2023-10-12 19:11:27 +0530

    Add files via upload

---
+++ Music web app/data.js
@@ -0,0 +1,36 @@
+let songs = [
+    {
+      name: 'Ghost',
+      path: 'Ghost.mp3',
+      artist: 'KMCR',
+      cover: 'albumPicture.png'
+    },
+  
+    {
+      name: 'Kyoto [Prototype]',
+      path: 'Kyoto [Prototype].mp3',
+      artist: 'KMCR',
+      cover: 'albumPicture.png'
+    },
+  
+    {
+      name: 'Mysterio [Prototype]',
+      path: 'Mysterio [Prototype].mp3',
+      artist: 'KMCR',
+      cover: 'albumPicture.png'
+    },
+  
+    {
+      name: 'Oriental',
+      path: 'Oriental.mp3',
+      artist: 'KMCR',
+      cover: 'albumPicture.png'
+    },
+  
+    {
+      name: 'Suburban [Prototype]',
+      path: 'Suburban [Prototype].mp3',
+      artist: 'KMCR',
+      cover: 'albumPicture.png'
+    }
+  ]
